0a0f2d0c51b9b51fed63eed9dba87e9f4acaf8ee
Curtis LaMalfa <curtis@kmni.com>
2013-07-23 10:51:39 -0700

    Updated URLS to link to pages.

--- inc/header.php
+++ inc/header.php
@@ -1,27 +1,27 @@
          <div class="header">
          	<div class="span4">
-      	  	 <a href="index.html" id="logotype"><img src="img/logotype.png"></a>
+      	  	 <a href="/index.php" id="logotype"><img src="/img/logotype.png"></a>
             </div>
             <div class="span3">
-      	    <a href="index.html" id="logo"><img src="img/logo.png"></a>
+      	    <a href="/index.php" id="logo"><img src="/img/logo.png"></a>
             </div>
              <div class="nav span5">
              	<div class="language">
-             <a href="#" id="english"><img src="img/english.png"></a>
-             <a href="#" id="dutch"><img src="img/dutch.png"></a>
+             <a href="#" id="english"><img src="/img/english.png"></a>
+             <a href="#" id="dutch"><img src="/img/dutch.png"></a>
              	</div>
       	    <ul class="links">
-                <li><a href="#">Stallions</a></li>
-                <li><a href="#">Mares</a></li>
-                <li><a href="#">Foals / Progeny</a></li>
-                <li><a href="#">Geldings</a></li>
-                <li><a href="#">Sale Horses</a></li>
+                <li><a href="/stallions.php">Stallions</a></li>
+                <li><a href="/mares.php">Mares</a></li>
+                <li><a href="/foals.php">Foals / Progeny</a></li>
+                <li><a href="/geldings.php">Geldings</a></li>
+                <li><a href="/sale-horses.php">Sale Horses</a></li>
       	    </ul>
       	    <ul class="links">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#">Show Schedule</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Contact Us</a></li>
+                <li><a href="/index.php">Home</a></li>
+                <li><a href="/show-schedule.php">Show Schedule</a></li>
+                <li><a href="/news.php">News</a></li>
+                <li><a href="/contact.php">Contact Us</a></li>
       	    </ul>
          	</div>  
          </div>  
